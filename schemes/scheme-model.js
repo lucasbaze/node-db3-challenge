d36d6059e4d2b56c189ca1f142ec78ae1d89e82b
Lucas Bazemore <lucasabazemore@gmail.com>
2019-10-09 08:45:46 -0500

    findSteps function

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -5,8 +5,33 @@ const db = knex(config.development);
 
 module.exports = {
     find,
+    findById,
+    findSteps,
 };
 
+//
+//Return array of all users
 function find() {
     return db('schemes');
 }
+
+//
+//Return a single Scheme based on scheme id
+function findById(id) {
+    return db('schemes')
+        .where({ id })
+        .first();
+}
+
+//
+//Return an array of steps based on scheme ID
+function findSteps(id) {
+    return db
+        .select('scheme_name', 'step_number', 'instructions')
+        .table('schemes')
+        .where('schemes.id', id)
+        .join('steps', 'schemes.id', 'steps.scheme_id');
+}
+
+//
+//
