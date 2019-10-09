7ceb7d0d2e0b87714ce1d91e270f673bcbf04065
Lucas Bazemore <lucasabazemore@gmail.com>
2019-10-09 08:31:33 -0500

    create config connection and find function

---
+++ schemes/scheme-model.js
@@ -0,0 +1,12 @@
+const knex = require('knex');
+const config = require('../knexfile.js');
+
+const db = knex(config.development);
+
+module.exports = {
+    find,
+};
+
+function find() {
+    return db('schemes');
+}
